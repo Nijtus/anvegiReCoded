3200c3efa1d2fbd85dc5ecc4a8e8b01715ba5766
Nijtus <justin.wein@hotmail.fr>
2020-02-21 15:22:53 +0100

    oc

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ token = fs.readFileSync("dataBase/token.txt", 'utf8');
 token = token.split('\n')
 
 strings = fs.readFileSync("string.json", 'utf8');
-strings = JSON.parse(strings)
+strings = JSON.parse(strings);
+
+oc = fs.readFileSync("dataBase/oc.json", 'utf8');
+oc = JSON.parse(oc);
+
+jeu = fs.readFileSync("jeu.json", 'utf8');
+jeu = JSON.parse(jeu);
 
 client.on('ready',function (){
 	console.log(strings["prete"]);
@@ -23,40 +29,83 @@ client.on('message',function(message){
 
   if(spl[0][0]=="!oc"){
 		var imageNom = "none"
-		if (message.attachments.first() !== undefined){
-			var type = message.attachments.first().filename;
+		if (spl.length == 1) {
 
-			if(type == "jpg"){imageNom=message.author.id+spl[0][1]+".jpg";}
-			else if (type == "png"){imageNom=message.author.id+spl[0][1]+".png";}
-			else if (type == "jpeg"){imageNom=message.author.id+spl[0][1]+".jpeg";}
-			console.log(imageNom);
-			if(imageNom != "none"){download(message.attachments.first().url,imageNom);}
-			else {message.channel.send(strings["error"]["fomatInvalide"])}
+			if (oc[spl[0][1]] !== undefined){
+				message.channel.send(oc[spl[0][1]]["description"])
+
+			}else{
+				message.channel.send(strings["error"]["nullOC"])
+
+			}
+
+		}else if (oc[spl[0][1]] !== undefined && message.author.id != oc[spl[0][1]]["proprietaire"]) {
+			message.channel.send(strings["error"]["accesDenied"])
+		}else {
+			if (message.attachments.first() !== undefined){
+				var type = message.attachments.first().filename;
+
+				if(type == "jpg"){imageNom=message.author.id+spl[0][1]+".jpg";}
+				else if (type == "png"){imageNom=message.author.id+spl[0][1]+".png";}
+				else if (type == "jpeg"){imageNom=message.author.id+spl[0][1]+".jpeg";}
+				console.log(imageNom);
+				if(imageNom != "none"){download(message.attachments.first().url,imageNom);}
+				else {message.channel.send(strings["error"]["fomatInvalide"])}
+
+			}
+
+			var description = "";
+			for (var x in spl) {
+				if (x!=0) {
+					description += spl[x] + "\n"
+				}
+			}
+
+	    result = {
+	      nom : spl[0][1],
+				proprietaire : message.author.id,
+	      description : description,
+	      image : imageNom
+	    }
+
+			oc[spl[0][1]]= result
+			saveOC();
+			message.channel.send(strings["enregistrementROC"])
+			console.log(oc);
 
 		}
-    result = {
-      nom : spl[0][1],
-      description : message.content.replace(spl[0][0]+" "+spl[0][1],""),
-      image : imageNom
-    }
-
-		let donnees = JSON.stringify(result)
-		fs.writeFile('dataBase/OC/'+message.author.id+'.json', donnees, function(erreur) {
-    	if (erreur) {
-        console.log(erreur)
-			}else{
-				message.channel.send(strings["enregistrementROC"])
-			}
-		});
   }
+
+	if(spl[0][0]=="!jeu"){
+		result = strings["jeu"].replace("${0}",jeu["contrainte"][getRandomInt(jeu["contrainte"].length)]).replace("${1}",jeu["objet"][getRandomInt(jeu["objet"].length)])
+
+		message.channel.send(result)
+	}
 });
 
+client.on("guildMemberAdd", (member) => {
+  member.send(strings["arrivee"].replace("${0}",member.name))
+});
+
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
 function download(url,nom){
 		console.log(url);
     request.get(url)
         .on('error', console.error)
         .pipe(fs.createWriteStream("dataBase/OC/"+nom));
+}
+
+function saveOC(){
+	let donnees = JSON.stringify(oc)
+	fs.writeFile('dataBase/oc.json', donnees, function(erreur) {
+		if (erreur) {
+	    console.log(erreur)
+		}
+	});
 }
 
 
